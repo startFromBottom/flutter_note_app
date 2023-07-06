b94ded014f976bd3c47d25c994486ae55b9fa635
startFromBottom <uhh0701@gmail.com>
2023-07-06 23:18:16 +0900

    26,27

---
+++ lib/presentation/notes/notes_view_model.dart
@@ -0,0 +1,53 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+import 'package:flutter/material.dart';
+import 'package:flutter_note_app/domain/model/note.dart';
+
+import 'package:flutter_note_app/domain/repository/note_repository.dart';
+import 'package:flutter_note_app/presentation/notes/notes_event.dart';
+import 'package:flutter_note_app/presentation/notes/notes_state.dart';
+
+class NotesViewModel with ChangeNotifier {
+  final NoteRepository repository;
+
+  NotesState _state = NotesState(notes: []);
+  NotesState get state => _state;
+
+  Note? _recentlyDeletedNote;
+
+  NotesViewModel({
+    required this.repository,
+  });
+
+  void onEvent(NotesEvent event) {
+    event.when(
+      loadNotes: _loadNotes,
+      deleteNote: _deleteNote,
+      restoreNote: _restoreNote,
+    );
+  }
+
+  Future<void> _loadNotes() async {
+    List<Note> notes = await repository.getNotes();
+    _state = state.copyWith(
+      notes: notes,
+    );
+    notifyListeners();
+  }
+
+  Future<void> _deleteNote(Note note) async {
+    await repository.deleteNote(note);
+
+    _recentlyDeletedNote = note;
+
+    await _loadNotes();
+  }
+
+  Future<void> _restoreNote() async {
+    if (_recentlyDeletedNote != null) {
+      await repository.insertNote(_recentlyDeletedNote!);
+      _recentlyDeletedNote = null;
+    }
+    _loadNotes();
+  }
+}
